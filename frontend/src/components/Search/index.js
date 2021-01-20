b010e62fe1d643aa51c278c630d58595ea9161b2
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-01-20 19:22:48 -0300

    Algumas melhorias

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Form, Jumbotron, Button } from 'react-bootstrap';
 import axios from 'axios';
 import BookNotFound from '../BookNotFound';
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
     Container,
@@ -12,12 +14,14 @@ import {
     TitleAuthor,
     ContainerDescription,
     LinkCompra,
-    ContainerJumbotron
+    ContainerJumbotron,
+    TitleName,
+    TitleAuthorName
 } from './styled';
 
 export default () => {
 
-    const [nameBook, setNameBook] = useState('');
+    const [nameBook, setNameBook] = useState('php');
     const [result, setResult] = useState([]);
     const [apiKey, setApiKey] = useState('AIzaSyCtu7ZTNOPAgMNlLrvGzHqCIaH-cU2AiIk');
 
@@ -34,6 +38,7 @@ export default () => {
         axios.get(`https://www.googleapis.com/books/v1/volumes?q=search+${nameBook}&key=${apiKey}&maxResults=40`)
             .then(data => {
                 setResult(data.data.items);
+                console.log(data.data.items);
             }).catch(erros => {
                 alert(erros);
             });
@@ -42,13 +47,32 @@ export default () => {
     if (result) {
         controlComponent = result.map(book => (
             <ContainerImageOne >
-                <LinkCompra href={book.saleInfo.buyLink} target="_blank">
-                    <ImageBook src={book.volumeInfo.imageLinks !== undefined ? book.volumeInfo.imageLinks.thumbnail : ''} alt={book.title} />
-                    {/* <ContainerDescription>
-                        <TitleBook>{book.volumeInfo.title}</TitleBook>
-                        <TitleAuthor>{book.volumeInfo.authors}</TitleAuthor>
-                    </ContainerDescription> */}
-                </LinkCompra>
+
+                <ImageBook src={book.volumeInfo.imageLinks !== undefined ? book.volumeInfo.imageLinks.thumbnail : ''} alt={book.title} />
+
+                <ContainerDescription>
+                    <TitleName>
+                        Livro:
+                    </TitleName>
+
+                    <TitleBook>{book.volumeInfo.title}</TitleBook>
+
+                    <TitleAuthorName>
+                        Autor:
+                    </TitleAuthorName>
+                    <TitleAuthor>
+                        {book.volumeInfo.authors && book.volumeInfo.authors}
+                        {!book.volumeInfo.authors && "Não Informado"}
+                    </TitleAuthor>
+
+                </ContainerDescription>
+                <hr />
+                {book.saleInfo.buyLink  &&
+                    
+                    <LinkCompra href={book.saleInfo.buyLink} target="_blank">
+                        <Button style={{alignItems: 'center', textAlign: 'center', marginLeft: '20px'}} variant="success">Comprar <FontAwesomeIcon icon={faShoppingCart} /></Button>
+                    </LinkCompra>
+                }
             </ContainerImageOne>
         ))
     } else {
@@ -57,17 +81,17 @@ export default () => {
 
     return (
         <Container>
-            <ContainerJumbotron>    
+            <ContainerJumbotron>
                 <Jumbotron style={{ paddingTop: "20px", paddingBottom: '20px' }}>
                     <Form onSubmit={handleSearch}>
                         <Form.Group>
-                            <Form.Label>Informe o nome do livro para pesquisar:</Form.Label>
+                            <Form.Label style={{ fontWeight: 'bold' }}>Pesquise seu livro:</Form.Label>
                             <Form.Control type="text" onChange={handleChange} value={nameBook} />
                         </Form.Group>
                         <Form.Group>
                             <Button variant="success" type="submit" data-testid="btn-pesquisar">
                                 Pesquisar
-                        </Button>
+                            </Button>
                         </Form.Group>
                     </Form>
                 </Jumbotron>
--- frontend/src/components/Search/styled.js
+++ frontend/src/components/Search/styled.js
@@ -2,8 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     padding: 20px;
-    background-color: #393e46;
-    height: 100%;
 `;
 
 export const ContainerJumbotron = styled.div`
@@ -11,7 +9,7 @@ export const ContainerJumbotron = styled.div`
 `;
 
 export const LinkCompra = styled.a`
-    
+    flex: 1;
 `;
 
 
@@ -19,30 +17,55 @@ export const ContainerImage = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+    width: 100%;
 `;
 
 export const ContainerImageOne = styled.div`
-    margin: 10px;
-    padding: 10px;
+    margin: 15px;
+    padding: 15px;
+    background-color: #E9E8EB;
+    
 `;
 
 export const ImageBook = styled.img`
-    width: 100px;
-    height: 100px;
-    border-radius: 5px;
+    width: 150px;
+    height: 150px;
+    border-radius: 15px;
+    
 `;
 
 export const TitleBook = styled.label`
-    font-size: 10px;
-    font-weight: bold;
+    width: 160%;
+    word-wrap: break-word;
+    font-size: 12px;
+    padding: 5px;
+    margin-bottom: 2px;
+    font-weight: 800;
 `;
 
 export const TitleAuthor = styled.label`
-    font-size: 10px;
-    font-weight: bold;
+    width: 160px;
+    font-size: 12px;
+    font-weight: 800;
+    padding: 5px;
+    margin-bottom: 2px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
 `;
 
 export const ContainerDescription = styled.div`
-
+    width: 100px;
+    word-wrap: break-word;
+    height: 130px;;
+    margin-top: 5px;
 `;
 
+export const TitleName = styled.label`
+    margin-bottom: 0px;
+    font-size: 12px;
+`;
+export const TitleAuthorName = styled.label`
+    margin-bottom: 0px;
+    font-size: 12px;
+`;
